f5b25da9f4d78313c8b237b0147917c8fa7dfc9f
Yeabsera K <yeabserak@gmail.com>
2017-10-02 13:42:52 -0400

    updated sound.py to return a max volume

--- modules/sound.py
+++ modules/sound.py
@@ -1,14 +1,18 @@
 import subprocess
 
-sample_time = 1
-
-while True:
-
-    command = "/usr/bin/arecord -D plughw:1,0 -d " + str(sample_time) + " -f S16_LE | /usr/bin/sox -t .wav - -n stat"
-
-    p = subprocess.Popen(command, bufsize=1, shell=True,  stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-    for line in p.stdout:
-        if "Maximum amplitude" in line:
-            print "Max:", line.split()[-1]
+# Get the max volume from the microphone
+# sample_time is in seconds
+def get_sound(sample_time=1):
+    for x in xrange(10): 
+        #command = "ssh cherry '/usr/bin/arecord -D plughw:1,0 -d " + str(sample_time) + " -f S16_LE | /usr/bin/sox -t .wav - -n stat'"
+        command = "/usr/bin/arecord -D plughw:1,0 -d " + str(sample_time) + " -f S16_LE | /usr/bin/sox -t .wav - -n stat"
+    
+        p = subprocess.Popen(command, bufsize=1, shell=True,  stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        for line in p.stdout:
+            if "Maximum amplitude" in line:
+                return float(line.split()[-1])
+        print "Audio Sensor Timeout. Attempting to reconnect"
+        time.sleep(1)
+     
+    print "Audio Sensor Failed"
 
